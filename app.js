ec6db6ea27c9693ef2385c822e0bbaf787703ef4
daniel <danielferre98@googlemail.com>
2023-03-17 12:06:01 +0000

    Added key coordinate generator

--- app.js
+++ app.js
@@ -64,10 +64,17 @@ const phrases = document.getElementById('phrases');
 const keyboardArea = document.getElementById('keyboard');
 if (keyboardArea != null) {
     kbArea = keyboardArea.getBoundingClientRect();
+
 }
 const keyboardInput = document.getElementById('keyboardInput');
+
+/*Store coordinates of most recent touch event*/
 let touchX;
 let touchY;
+
+/*Store key coordinates*/
+let keyX = [];
+let keyY = [];
 
 /*Check that kbSeshCount isn't null, otherwise other pages will fail to work*/
 if (kbSeshCount != null) {
@@ -87,12 +94,54 @@ if (phraseCounter != null) {
 /*Generate keyboard and start practice session*/
 
 if (window.location.pathname.includes('experiment.html')) {
-    generateKb();
     practiceSession();
+    generateKeyCoords();
+}
+
+/*Generate coordinates for keys on standard keyboard*/
+function generateKeyCoords() {
+    let topRow = document.getElementsByClassName('topKeys');
+    let topRowBounds = document.querySelector('.topRow').getBoundingClientRect();
+    let secRow = document.getElementsByClassName('secKeys');
+    let secRowBounds = document.querySelector('.secondRow').getBoundingClientRect();
+    let thiRow = document.getElementsByClassName('thiKeys');
+    let thiRowBounds = document.querySelector('.thirdRow').getBoundingClientRect();
+    let botRow = document.getElementsByClassName('spaceKey');
+    let botRowBounds = document.querySelector('.bottomRow').getBoundingClientRect();
+    let rowList = [topRow, secRow, thiRow, botRow];
+    let rowBoundList = [topRowBounds, secRowBounds, thiRowBounds, botRowBounds];
+    let xCoord = 0;
+    let yCoord = 0;
+    for (let rows = 0; rows < rowList.length; rows++) {
+        /*Iterate through rows and generate y coodinates*/
+        
+        let currentRow = rowList[rows];
+        let currentRowBounds = rowBoundList[rows];
+        console.log(currentRowBounds.width);
+        let keyHeight = currentRowBounds.height;
+        let keyWidth = currentRowBounds.width / currentRow.length;
+        keyY.push(yCoord);
+        xCoord = (kbArea.width - currentRowBounds.width) / 2;
+        console.log('xCoord = ' + xCoord);
+        yCoord = yCoord + keyHeight;
+        for (let keys = 0; keys < currentRow.length; keys++) {
+            /*Iterate through keys of current row and generate x coordinates*/
+            
+            keyX.push(xCoord);
+            xCoord = xCoord + keyWidth;
+        }
+        /*Won't include edge coordinates in the arrays, deal with it in the keyPressed function*/
+    }
+    console.log(keyX);
+    console.log(keyY);
+
+    /*Map values in keyX and keyY to the keys*/
+
 }
 
 function practiceSession() {
     
+    /*Call generateKb when practice session is finished*/
 }
 
 /*Select 1 or 2 to give the user either the control or the test keyboard 1 = Control, 2 = Test*/
